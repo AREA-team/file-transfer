5c248c6670ce52453b9b3087c9a775212e910a55
vacya01 <nikulin.vasily.777@ya.ru>
2021-05-22 10:42:58 +0300

    22.05.2021 examples

--- client.py
+++ client.py
@@ -5,6 +5,11 @@ sock.connect(('localhost', 9090))
 sock.send(b'hello, world!')
 
 data = sock.recv(1024)
+print(data)
+
+sock.send(b'gg!')
+
+data = sock.recv(1024)
 sock.close()
 
 print(data)
--- server.py
+++ server.py
@@ -2,15 +2,13 @@ import socket
 
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(1)
-conn, address = sock.accept()
-
-print('connected:', address)
 
 while True:
+    sock.listen(1)
+    conn, address = sock.accept()
+    print('connected:', address)
     data = conn.recv(1024)
-    if not data:
-        break
     conn.send(data.upper())
-
-conn.close()
+    while data:
+        data = conn.recv(1024)
+        conn.send(data.upper())
